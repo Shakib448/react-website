4e66063d33a11c5cf19ca1d34c405d9cfab0243e
Shakib448 <shakiba448@gmail.com>
2020-08-07 20:00:37 +0600

    Updates

---
+++ precache-manifest.bb0276cd62c6e4436b8c8ce93bf6cd03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1bb5e411480471e1c01dfd437fcc09",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "ea35df4c0863db7fe074",
+    "url": "/react-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "c022108f8c9e67750df0",
+    "url": "/react-website/static/css/main.28ae54c1.chunk.css"
+  },
+  {
+    "revision": "ea35df4c0863db7fe074",
+    "url": "/react-website/static/js/2.71e80448.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-website/static/js/2.71e80448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c022108f8c9e67750df0",
+    "url": "/react-website/static/js/main.dc1ae054.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  },
+  {
+    "revision": "c12c29610ecf4730f293ed69e662b9ba",
+    "url": "/react-website/static/media/Mask Group-1.c12c2961.png"
+  },
+  {
+    "revision": "b2a0d5f0deeba94cf7e02202ac7b83d5",
+    "url": "/react-website/static/media/Mask Group-2.b2a0d5f0.png"
+  },
+  {
+    "revision": "7f55e8ff895b544fe2d6cf9c1aa1643a",
+    "url": "/react-website/static/media/Mask Group-3.7f55e8ff.png"
+  },
+  {
+    "revision": "302f793da6bead8773878509eb527d3e",
+    "url": "/react-website/static/media/Mask Group-4.302f793d.png"
+  },
+  {
+    "revision": "e5cebc3ade37c0e2e4361948ada16756",
+    "url": "/react-website/static/media/Mask Group.e5cebc3a.png"
+  }
+]);
